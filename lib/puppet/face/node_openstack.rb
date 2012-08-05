d86f6b215d97ac38d1ddd944851749c5ce029c58
Zack Smith <zack.smith@puppetlabs.com>
2012-08-05 02:52:41 +0000

    Remove endpoint option

--- lib/puppet/face/node_openstack.rb
+++ lib/puppet/face/node_openstack.rb
@@ -4,18 +4,6 @@ Puppet::Face.define(:node_openstack, '0.0.1') do
   copyright "Puppet Labs", 2011
   license   "Apache 2 license; see COPYING"
 
-  option "--endpoint ENDPOINT" do
-
-    summary 'Nova API server that services vm management requests'
-    description <<-EOT
-      Nova API server that services vm management requests.
-      By default, this should be : http://your.api.server:8773/services/Cloud
-    EOT
-
-    required
-
-  end
-
   summary "View and manage openstack nodes."
   description <<-'EOT'
     This subcommand provides a command line interface to manage Openstack
